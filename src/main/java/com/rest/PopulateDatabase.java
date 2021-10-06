91b6cae634ea4041e091bee40f86a4cb635b1dd9
Felix <felix.stapf@web.de>
2021-10-06 16:18:46 +0200

    Initial commit

---
+++ src/main/java/com/rest/PopulateDatabase.java
@@ -0,0 +1,20 @@
+package com.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PopulateDatabase {
+    private static final Logger log = LoggerFactory.getLogger(PopulateDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+        };
+    }
+}
